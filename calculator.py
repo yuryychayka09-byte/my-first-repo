30ee606679eb7f6c75f4ccfdccda9f7f22f02c62
Chaika Yurii <yuryychayka09@gmail.com>
2025-11-12 21:27:35 +0200

    Added modulo dilennia operation to calculator

--- calculator.py
+++ calculator.py
@@ -21,18 +21,25 @@ def power(a, b):
     """Піднесення числа a до степеня b"""
     return a ** b
 
+def modulo(a, b):
+    """Остача від ділення"""
+    if b != 0:
+        return a % b
+    else:
+        return "Помилка: ділення на нуль!"
+
 print("=== Простий калькулятор ===")
-print("Операції: +, -, *, /, **")
+print("Операції: +, -, *, /, **, %")
 print("Для виходу введіть 'exit'")
 
 while True:
-    operation = input("\nВведіть операцію (+, -, *, /, **) або 'exit': ")
+    operation = input("\nВведіть операцію (+, -, *, /, **, %) або 'exit': ")
 
     if operation.lower() == 'exit':
         print("Good bye!")
         break
 
-    if operation not in ['+', '-', '*', '/', '**']:
+    if operation not in ['+', '-', '*', '/', '**', '%']:
         print("Невірна операція!")
         continue
 
@@ -50,6 +57,8 @@ while True:
             result = divide(num1, num2)
         elif operation == '**':
             result = power(num1, num2)
+        elif operation == '%':
+            result = modulo(num1, num2)
 
         print(f"Результат: {result}")
 
